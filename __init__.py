3fe8647bf9436f3bd317e659e19ffd4d57080216
zfergus <zfergus@nyu.edu>
2018-12-16 18:43:49 -0500

    Updated __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 """OpenFace for PyTorch using the PyTorch weights `net.pth`."""
+from . import net
 
 import pathlib
-import net
 
 openface_model_path = str(pathlib.Path(__file__).resolve().parent / "net.pth")
