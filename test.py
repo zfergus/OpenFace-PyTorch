2d39961490e707ecd382df0f2a9b8216f0d6d298
zfergus <zfergus@nyu.edu>
2018-12-16 21:10:54 -0500

    Updated test script

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-import torch
-from torch.autograd import Variable
-import net
 import pathlib
+import torch
+# from torch.autograd import Variable
 from torchvision import transforms, datasets
 from PIL import Image
 
+from . import net
+
 containing_dir = pathlib.Path(__file__).resolve().parent
+face_patch_dir = containing_dir.parents[1] / "images/faces/patches"
 
 model = net.model
 model.load_state_dict(torch.load(containing_dir / "net.pth"))
@@ -24,25 +26,37 @@ def load_image_tensor(filename, batch_size, image_shape=None):
     return image_transform(image)[:3].repeat(batch_size, 1, 1, 1)
 
 
-original = load_image_tensor(
-    containing_dir.parent / "images/faces/original.png", 1)
-bw = load_image_tensor(
-    containing_dir.parent / "images/faces/bw.png", 1)
-stylized = load_image_tensor(
-    containing_dir.parent / "images/faces/stylized.png", 1)
-other = load_image_tensor(
-    containing_dir.parent / "images/faces/other.png", 1)
+original = load_image_tensor(face_patch_dir / "original.png", 1)
+bw = load_image_tensor(face_patch_dir / "bw.png", 1)
+stylized_face_false = load_image_tensor(
+    face_patch_dir / "stylized-face=false.png", 1)
+stylized_face_true_1 = load_image_tensor(
+    face_patch_dir / "stylized-face=true-1.png", 1)
+stylized_face_true_3 = load_image_tensor(
+    face_patch_dir / "stylized-face=true-3.png", 1)
+other = load_image_tensor(face_patch_dir / "other.png", 1)
 
 # CPU
 f1 = model(original)
 f2 = model(bw)
-f3 = model(stylized)
-f4 = model(other)
+f3 = model(stylized_face_false)
+f4 = model(stylized_face_true_1)
+f5 = model(stylized_face_true_3)
+f6 = model(other)
 
-print(torch.nn.functional.mse_loss(f1, f1))
-print(torch.nn.functional.mse_loss(f1, f2))
-print(torch.nn.functional.mse_loss(f1, f3))
-print(torch.nn.functional.mse_loss(f1, f4))
+print("MSE(original, original)                                = {:.4f}".format(
+    torch.nn.functional.mse_loss(f1, f1).data.item()))
+print("MSE(original, black and white)                         = {:.4f}".format(
+    torch.nn.functional.mse_loss(f1, f2).data.item()))
+print("MSE(original, stylized without facial preservation)    = {:.4f}".format(
+    torch.nn.functional.mse_loss(f1, f3).data.item()))
+print("MSE(original, stylized with heavy facial preservation) = {:.4f}".format(
+    torch.nn.functional.mse_loss(f1, f4).data.item()))
+print("MSE(original, stylized with light facial preservation) = {:.4f}".format(
+    torch.nn.functional.mse_loss(f1, f5).data.item()))
+print("MSE(original, other)                                   = {:.4f}".format(
+    torch.nn.functional.mse_loss(f1, f6).data.item()))
+
 
 breakpoint()
 # print('CPU done')
